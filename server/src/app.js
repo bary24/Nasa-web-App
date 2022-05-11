5290b3f41d8f4b1d75acd4e17b1f8259938b74b3
Ahmed Abdelbary <ahmed.bary@icloud.com>
2022-05-11 17:09:15 +0200

    Adding api versioning

--- server/src/app.js
+++ server/src/app.js
@@ -3,16 +3,14 @@ const cors=require("cors");
 const path=require("path");
 const morgan=require("morgan");
 const app=express();
-const launchesRouter=require("./routes/launches/launches.router")
-const planetsRouter=require("./routes/planets/planets.router");
+const api=require("./routes/api");
 app.use(cors({
     origin:"http://localhost:3000"
 }));
 app.use(express.json());
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname,"..","public")));
-app.use("/planets",planetsRouter);
-app.use("/launches",launchesRouter);
+app.use("/v1",api);
 app.get("/*",function(req,res){
     res.sendFile(path.join(__dirname,"..","public","index.html"))
 });
